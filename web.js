d158342fc02d9e94862c472a002adafaff7f32ef
unendedquest <cpesquita@gmail.com>
2013-09-03 09:38:29 +0000

    altered web.js to load css

--- web.js
+++ web.js
@@ -5,6 +5,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+app.configure(function () {
+    app.use(
+        "/", //the URL throught which you want to access to you static content
+        express.static(__dirname) //where your static content is located in your filesystem
+    );
+})
+;
+
 app.get('/', function(request, response) {
   response.send(buf.toString());
 });
